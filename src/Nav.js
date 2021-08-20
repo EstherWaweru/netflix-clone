6d6f394900afacb6ab9fa37fa9fd9ef99ca75f90
EstherWaweru <estherwaweru86@gmail.com>
2021-08-20 16:07:50 +0300

    Adding animation to navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,21 @@
-import React from 'react'
-import './Nav.css'
+import React, { useEffect, useState } from 'react'
+import './Nav.css';;
 function Nav(){
+    const[show,handleShow] = useState(false);
+    useEffect(() =>{
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) { 
+                handleShow(true);
+            }else{
+                handleShow(false);
+            }
+        });
+        return () => {
+           window.removeEventListener("scroll");
+        };
+    },[]);
     return(
-        <div className="nav">
+        <div className={`nav ${show && "nav_black"}`}>
         <img className="nav_logo"
         //src="https://i.pinimg.com/originals/4a/5c/7d/4a5c7dc0c88598093e5449b392a1b4b5.png"
         src=" https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
